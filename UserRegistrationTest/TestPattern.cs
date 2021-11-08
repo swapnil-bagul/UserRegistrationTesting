4f6b5a58c0106eb560cf92782082c8662600cca6
swapnil-bagul <bagulswapnilp@gmail.com>
2021-11-08 18:40:50 +0530

    [Swapnil] Added UC1 User FirstName Code

---
+++ UserRegistrationTest/TestPattern.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserRegistrationTesting;
+using System;
+
+namespace UserRegistrationTest
+{
+    [TestClass]
+    public class TestPattern
+    {
+        /// <summary>
+        /// UC1 FirstName of User
+        /// first letter of name should be capital
+        /// </summary>
+        [TestMethod]
+        public void FirstName()
+        {
+            //arrange
+            string message = "Swapnil";
+            string expected = "valid";
+
+            //act
+            UserRegistration userRegistration = new UserRegistration(message);
+            string actual=userRegistration.PatternChecking();
+
+            //Assert
+            Assert.AreEqual(expected, actual );
+
+
+        }
+    }
+}
---
+++ UserRegistrationTesting/UserRegistration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationTesting
+{
+    public class UserRegistration
+    {
+        //Instance Variable
+        public string message;
+
+        //Parameterise Constructor
+        public UserRegistration (string message)
+        {
+            this.message = message;
+        }
+
+        //create pattern 
+        string pattern = "^[A-Z]{1}[a-z]{2,}$";
+
+        public string PatternChecking()
+        {
+            //used Regex predefined class
+            Regex regex = new Regex(pattern);
+            //used IsMatch Predefined Method
+            if(regex.IsMatch (message))
+            {
+                return "valid";
+            }
+            else
+            {
+                return "invalid";
+            }
+        }
+       
+    }
+}
